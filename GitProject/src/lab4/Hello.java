e8cac82012db43a8ab42ab4c25686c555dcbf9aa
Isaacpruner <Isaacpruner@cb5107-09.labs.sce.carleton.ca>
2017-10-06 08:43:02 -0400

    Version 1

---
+++ GitProject/src/lab4/Hello.java
@@ -0,0 +1,18 @@
+package lab4;
+
+public class Hello {
+
+	public static void main(String[] args) 
+	{
+		Hello hello = new Hello();
+		hello.methodA();
+		hello.methodB();
+		hello.methodC();
+	}
+
+	public void methodA( ) { }
+
+	public void methodB() { }
+
+	public void methodC() { }
+}
